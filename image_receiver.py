5a55e7ba76044aaadf73886fd0e35d8bebdfd5d3
mhjasonliu <mhjasonliu@gmail.com>
2020-03-11 14:47:00 -0500

    slightly faster operations

--- image_receiver.py
+++ image_receiver.py
@@ -11,16 +11,17 @@ class ImageReceiver:
         buffer = []
         while True:
             idx_buffer = []
-            buffer.extend(self.ser.read(self.ser.inWaiting()))
+            buffer += self.ser.read(self.ser.inWaiting())
             for idx in range(0, len(buffer) - 1):
                 if buffer[idx] == 0xff and buffer[idx + 1] == 0xd8:
                     idx_buffer.append(idx)   
     
             if len(idx_buffer) > 1:
                 image = buffer[idx_buffer[0]:idx_buffer[1]]
-                print(len(bytes(image)))
+                im_bytes = bytes(image)
+                print(len(im_bytes))
 
-                im = cv2.imdecode(np.frombuffer(bytes(image),dtype='uint8'),
+                im = cv2.imdecode(np.frombuffer(im_bytes,dtype='uint8'),
                                   cv2.IMREAD_UNCHANGED)
 
                 return im
